c5ae4790ec814fd73e3afc7e1440caa88e085c02
Michele Sciabarra <msciabarra@apache.org>
2025-06-15 17:11:00 +0200

    utils/caddy/update.sh

--- utils/caddy/update.sh
+++ utils/caddy/update.sh
@@ -1,3 +1,8 @@
 bash getusers.sh eri
 bash getusers.sh draco
 cat eri/* draco/* >_users.caddy
+sudo cp _users.caddy /etc/caddy/_users.caddy
+sudo cp Caddyfile /etc/caddy/Caddyfile
+sudo chmod 644 /etc/caddy/*
+sudo systemctl restart caddy
+
