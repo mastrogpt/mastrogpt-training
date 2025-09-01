e48abf9cd68f4f1833193d7cbad1cef3b1077a97
Michele Sciabarra <msciabarra@apache.org>
2025-09-01 09:04:07 +0200

    utils

--- utils/caddy/update.sh
+++ utils/caddy/update.sh
@@ -1,6 +1,7 @@
 bash getusers.sh eri
 bash getusers.sh draco
 cat eri/* draco/* >_users.caddy
+echo "demo $(caddy hash-password --plaintext salmone)" >>_users.caddy
 sudo cp _users.caddy /etc/caddy/_users.caddy
 sudo cp Caddyfile /etc/caddy/Caddyfile
 sudo chmod 644 /etc/caddy/*
