7088beba8e195caf0932f5f97ce70509cb31f0eb
Michele Sciabarra <msciabarra@apache.org>
2025-09-23 13:16:56 +0200

    users

--- utils/caddy/update.sh
+++ utils/caddy/update.sh
@@ -1,9 +1,13 @@
+# saves users2.txt in 1pw ollama users2.txt
+: > _users2.caddy
+while IFS=: read -r user pass; do
+  echo "$user $(caddy hash-password --plaintext "$pass")" >> "_users2.caddy"
+done < "users2.txt"
 bash getusers.sh eri
 bash getusers.sh draco
 cat eri/* draco/* >_users.caddy
-echo "demo $(caddy hash-password --plaintext salmone)" >>_users.caddy
+sudo cp _users2.caddy /etc/caddy/_users2.caddy
 sudo cp _users.caddy /etc/caddy/_users.caddy
 sudo cp Caddyfile /etc/caddy/Caddyfile
 sudo chmod 644 /etc/caddy/*
 sudo systemctl restart caddy
-
